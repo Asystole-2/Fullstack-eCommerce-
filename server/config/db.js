2f1906aa613a14d0e73486822947fc41d06e04af
Luke <lukekirwan39@gmail.com>
2025-01-24 21:31:42 +0000

    New changelist

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 mongoose.connect(`mongodb://localhost/${process.env.DB_NAME}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
+});
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
