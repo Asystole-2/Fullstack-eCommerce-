c87d365fbab406f662fac08c19e1b7a591bf8ef7
rogueinc7 <davidailemen7@gmail.com>
2025-01-28 19:26:19 +0000

    Added a new Register class which is linked to Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,34 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 class Login extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             loginEmail: '',
             loginPassword: '',
-            registerEmail: '',
-            registerPassword: '',
-            confirmPassword: '',
-        }
+        };
     }
 
     handleLogin = (e) => {
-        e.preventDefault()
-        console.log('Login with:', this.state.loginEmail, this.state.loginPassword)
-    }
-
-    handleRegister = (e) => {
-        e.preventDefault()
-        if (this.state.registerPassword !== this.state.confirmPassword) {
-            alert('Passwords do not match')
-            return
-        }
-        console.log('Register with:', this.state.registerEmail, this.state.registerPassword)
-    }
+        e.preventDefault();
+        console.log('Login with:', this.state.loginEmail, this.state.loginPassword);
+    };
 
     render() {
         return (
-            <div style={{ display: 'flex', justifyContent: 'space-between', padding: 20 }}>
+            <div>
                 <form onSubmit={this.handleLogin}>
                     <h2>Login</h2>
                     <label>
@@ -44,27 +32,11 @@ class Login extends React.Component {
                     </div>
                     <button type="submit">Log in</button>
                     <a href="#">Lost your password?</a>
-                </form>
-
-                <form onSubmit={this.handleRegister}>
-                    <h2>Register</h2>
-                    <label>
-                        Email Address *
-                        <input type="email" value={this.state.registerEmail} onChange={e => this.setState({ registerEmail: e.target.value })} required />
-                    </label>
-                    <label>
-                        Password *
-                        <input type="password" value={this.state.registerPassword} onChange={e => this.setState({ registerPassword: e.target.value })} required />
-                    </label>
-                    <label>
-                        Confirm Password *
-                        <input type="password" value={this.state.confirmPassword} onChange={e => this.setState({ confirmPassword: e.target.value })} required />
-                    </label>
-                    <button type="submit">Register</button>
+                    <p>Don't have an account? <a href="/register">Create one</a></p>
                 </form>
             </div>
-        )
+        );
     }
 }
 
-export default Login
+export default Login;
---
+++ client/src/components/Register.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+class Register extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            registerEmail: '',
+            registerPassword: '',
+            confirmPassword: '',
+        };
+    }
+
+    handleRegister = (e) => {
+        e.preventDefault();
+        if (this.state.registerPassword !== this.state.confirmPassword) {
+            alert('Passwords do not match');
+            return;
+        }
+        console.log('Register with:', this.state.registerEmail, this.state.registerPassword);
+    };
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleRegister}>
+                    <h2>Register</h2>
+                    <label>
+                        Email Address *
+                        <input type="email" value={this.state.registerEmail} onChange={e => this.setState({ registerEmail: e.target.value })} required />
+                    </label>
+                    <label>
+                        Password *
+                        <input type="password" value={this.state.registerPassword} onChange={e => this.setState({ registerPassword: e.target.value })} required />
+                    </label>
+                    <label>
+                        Confirm Password *
+                        <input type="password" value={this.state.confirmPassword} onChange={e => this.setState({ confirmPassword: e.target.value })} required />
+                    </label>
+                    <button type="submit">Register</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Register;
