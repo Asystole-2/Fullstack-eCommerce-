1efba9b6c0aafde5061f7f1824bd6c62b0e0b6a7
Luke <lukekirwan39@gmail.com>
2025-02-08 22:21:01 +0000

    Password encryption and admin access

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from "./Navbar";
+import axios from "axios";
 
 class Login extends React.Component {
     constructor(props) {
@@ -7,85 +8,81 @@ class Login extends React.Component {
         this.state = {
             loginEmail: '',
             loginPassword: '',
-            registerEmail: '',
-            registerPassword: '',
-            confirmPassword: '',
             role: 'user',
         }
     }
 
-    handleLogin = (e) => {
+    handleLogin = async (e) => {
         e.preventDefault()
         console.log('Login with:', this.state.loginEmail, this.state.loginPassword)
         // Add login logic here
+        try {
+            const res = await axios.post('http://localhost:8000/login', this.state.loginEmail, this.state.loginPassword)
+            localStorage.setItem('token', res.data.token)
+            this.setState({role: res.data.role})
+            alert('Login successfully')
+        } catch (error) {
+            alert('Login failed.')
+        }
     }
 
     handleRoleChange = e => {
         this.setState({role: e.target.value})
     }
 
-    handleRegister = (e) => {
-        e.preventDefault()
-        if (this.state.registerPassword !== this.state.confirmPassword) {
-            alert('Passwords do not match')
-            return
-        }
-        console.log('Register with:', this.state.registerEmail, this.state.registerPassword)
-        // Add registration logic here
-    }
-
     render() {
         return (
             <div>
-            <Navbar/>
-            <div className="login">
-                <div className="login-container">
-                    {/* Login Form */}
-                    <form onSubmit={this.handleLogin}>
-                        <h2>Login</h2>
-                        <div className="input-group">
-                            <label>
-                                Email Address *
-                                <input type="email" value={this.state.loginEmail}
-                                       onChange={e => this.setState({loginEmail: e.target.value})} required/>
-                            </label>
-                            <label>
-                                Password *
-                                <input type="password" value={this.state.loginPassword}
-                                       onChange={e => this.setState({loginPassword: e.target.value})} required/>
-                            </label>
-                            <select value={this.state.role} onChange={this.handleRoleChange}>
-                                <option value="user">User</option>
-                                <option value="admin">Admin</option>
-                            </select>
-                            <div>
-                                <input type="checkbox"/>Remember Me
+                <Navbar/>
+                <div className="login">
+                    <div className="login-container">
+                        {/* Login Form */}
+                        <form onSubmit={this.handleLogin}>
+                            <h2>Login</h2>
+                            <div className="input-group">
+                                <label>
+                                    Email Address *
+                                    <input type="email" value={this.state.loginEmail}
+                                           onChange={e => this.setState({loginEmail: e.target.value})} required/>
+                                </label>
+                                <label>
+                                    Password *
+                                    <input type="password" value={this.state.loginPassword}
+                                           onChange={e => this.setState({loginPassword: e.target.value})} required/>
+                                </label>
+                                <select value={this.state.role} onChange={this.handleRoleChange}>
+                                    <option value="user">User</option>
+                                    <option value="admin">Admin</option>
+                                </select>
+                                <div>
+                                    <input type="checkbox"/>Remember Me
+                                </div>
+                                <button type="submit" onClick={this.handleLogin}>Log in</button>
+                                <a href="#">Lost your password?</a>
                             </div>
-                            <button type="submit">Log in</button>
-                            <a href="#">Lost your password?</a>
-                        </div>
-                    </form>
+                        </form>
 
-                    {/* Displaying different content based on role option */}
-                    <div style={{marginLeft: 20}}>
-                        {this.state.role === 'user' && (
-                            <div>
-                                <h3>Welcome, User!</h3>
-                                <p className="switch">Access your personalized dashboard and manage your account.</p>
-                                <br/>
-                                <p className="switch">Don't have an account?</p>
-                                <a href="/Register.js">Register</a>
-                            </div>
-                        )}
-                        {this.state.role === 'admin' && (
-                            <div>
-                                <h3>Welcome, Admin!</h3>
-                                <p>Manage users, view analytics, and configure settings.</p>
-                            </div>
-                        )}
+                        {/* Displaying different content based on role option */}
+                        <div style={{marginLeft: 20}}>
+                            {this.state.role === 'user' && (
+                                <div>
+                                    <h3>Welcome, User!</h3>
+                                    <p className="switch">Access your personalized dashboard and manage your
+                                        account.</p>
+                                    <br/>
+                                    <p className="switch">Don't have an account?</p>
+                                    <a href="/Register.js">Register</a>
+                                </div>
+                            )}
+                            {this.state.role === 'admin' && (
+                                <div>
+                                    <h3>Welcome, Admin!</h3>
+                                    <p>Manage users, view analytics, and configure settings.</p>
+                                </div>
+                            )}
+                        </div>
                     </div>
                 </div>
-            </div>
             </div>
         )
     }
--- server/server.js
+++ server/server.js
@@ -4,6 +4,17 @@ require(`dotenv`).config({path:`./config/.env`})
 //mongoose stuff
 require(`./config/db`)
 
+const mongoose = require('mongoose');
+
+// Cors
+const cors = require('cors')
+
+// Bcrypt
+const bcrypt = require('bcrypt')
+
+// jsonwebtoken
+const jwt = require('jsonwebtoken')
+
 // Express
 const express = require(`express`)
 const app = express()
@@ -30,9 +41,60 @@ app.use((req, res, next) => {next(createError(404))})
 app.use(function (err, req, res, next)
 {
     console.error(err.message)
-    if (!err.statusCode) 
+    if (!err.statusCode)
     {
         err.statusCode = 500
     }
     res.status(err.statusCode).send(err.message)
+})
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    role: {type: String, default: "user"}
+})
+
+const User = mongoose.model('User', UserSchema)
+
+// Middleware to verify JWT Token
+const verifyToken = (req, res, next) => {
+    const token = req.headers['Authorization']
+    if (!token) return res.status(401).json('Access denied')
+
+    try {
+        const verified = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = verified
+        next()
+    }catch(err) {
+        res.status(400).json({error: 'Invalid token'})
+    }
+}
+
+app.post('/Register', async (req, res) => {
+    const { username, password, role } = req.body
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(password, salt)
+
+    const user = new User({username, password: hashPassword, role})
+    await user.save()
+    res.json({ message: 'Registered successfully' })
+})
+
+app.post('/Login', async (req, res) => {
+    const { username, password } = req.body
+
+    const user = await User.findOne({ username })
+    if (!user) return res.status(400).json({ error: 'User not found' })
+
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) return res.status(400).json({ error: 'Wrong Password' })
+
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token, role: user.role });
+    res.json({ message: 'Registered successfully' })
+})
+
+app.get('/Admin', verifyToken, (req, res) => {
+    if (req.user.role !== 'admin') return res.status(403).json({ error: 'Access Denied' })
+    res.json({ message: 'Welcome Admin!' });
 })
