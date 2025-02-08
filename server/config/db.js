f7cade8b455804bfda726fa947339f6f8253095a
Luke <lukekirwan39@gmail.com>
2025-02-08 19:20:16 +0000

    Updates

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,24 @@
 const mongoose = require('mongoose')
-mongoose.connect(`mongodb://localhost/${process.env.DB_NAME}`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+
+const uri = 'mongodb://localhost/fullstack_ecommerce_project';
+
+mongoose.connect(uri)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', () => {console.log("connected to", db.client.s.url)})
+
+
+async function connectDB() {
+    try {
+        await mongoose.connect(uri); // No need for useNewUrlParser or useUnifiedTopology
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1); // Stop app if connection fails
+    }
+}
+
+module.exports = connectDB;
