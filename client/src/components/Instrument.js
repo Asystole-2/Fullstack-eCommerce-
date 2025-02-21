f7c573961fa7be761335a8b98320c6bc42cd7e4b
lukekirwan <lukekirwan39@gmail.com>
2025-02-21 13:14:53 +0000

    Fixes so it can merge with main

--- client/src/components/Instrument.js
+++ client/src/components/Instrument.js
@@ -1,51 +1,31 @@
-// import React, {Component} from "react"
-// import {Link} from "react-router-dom"
-//
-//
-// export default class instrument extends Component
-// {
-//     render()
-//     {
-//         const { product, onDelete, onUpdate } = this.props
-//         return (
-//             <div className="product-card">
-//                 <img src={product.image} alt={product.name} />
-//                 <h2>{product.name}</h2>
-//                 <p>{product.description}</p>
-//                 <p>Price: ${product.price.toFixed(2)}</p>
-//                 <p>Stock: {product.stock}</p>
-//                 <button onClick={() => onUpdate({ ...product, stock: product.stock - 1 })}>
-//                     Decrease Stock
-//                 </button>
-//                 <button onClick={() => onDelete(product.id)}>Delete</button>
-//             </div>
-//         )
-//     }
-//
-// }
-import React, { Component } from "react"
+import React, {Component} from "react"
+import {Link} from "react-router-dom";
 
 export default class Instrument extends Component {
     handleDeleteClick = () => {
-        const { product, onDelete } = this.props
+        const {product, onDelete} = this.props
         onDelete(product._id)
     }
 
     render() {
-        const { product, onUpdate } = this.props
-        const value = product.price || 0;
+        const {product, onUpdate} = this.props
+        const value = product.price || 0
         return (
             <div className="product-card">
-                <img src={product.image} alt={product.name} />
+                <img src={product.image} alt={product.name}/>
                 <h2>{product.name}</h2>
                 <p>{product.description}</p>
                 <p>Price: ${value.toFixed(2)}</p>
                 <p>Stock: {product.stock}</p>
 
-                <button onClick={() => onUpdate({ ...product, stock: product.stock - 1 })}>
+                <button onClick={() => onUpdate({...product, stock: product.stock - 1})}>
                     Decrease Stock
                 </button>
+                <button onClick={() => onUpdate({...product, stock: product.stock + 1})}>
+                    Increase Stock
+                </button>
                 <button onClick={this.handleDeleteClick}>Delete</button>
+                <Link to={"/EditInstrument/" + product._id}>Edit</Link>
             </div>
         )
     }
