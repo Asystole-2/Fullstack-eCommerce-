e3752e08341c528183aecfb3d66b98dbecc94aba
Asystole-2 <153837128+Asystole-2@users.noreply.github.com>
2025-02-25 00:52:14 +0000

    improved filtering: all option now displays all products before filters are applied sorting: sorts products by name or reviews or price

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,10 +1,11 @@
-import React, {Component} from "react"
+import React, { Component } from "react"
 import AddInstrument from "./AddInstrument"
 import Instrument from "./Instrument"
 import CategoryDropDown from "./CategoryDropDown"
-import BrandDropDown from "./BrandDropDown";
+import BrandDropDown from "./BrandDropDown"
+import SortProducts from "./SortProducts"
 
-import {SERVER_HOST} from "../config/global_constants"
+import { SERVER_HOST } from "../config/global_constants"
 import axios from "axios"
 
 export default class Products extends Component {
@@ -15,11 +16,10 @@ export default class Products extends Component {
             products: [],
             searchTerm: '',
             brands: [],
-            selectedBrand: '',
+            selectedBrand: 'All',
             categories: [],
             selectedCategory: 'All',
-            selectedProducts: 'All',
-
+            sortOrder: 'default',
         }
 
         this.handleAddProduct = this.handleAddProduct.bind(this)
@@ -27,92 +27,95 @@ export default class Products extends Component {
         this.handleUpdateProduct = this.handleUpdateProduct.bind(this)
         this.handleCategoryChange = this.handleCategoryChange.bind(this)
         this.handleBrandChange = this.handleBrandChange.bind(this)
+        this.handleSortChange = this.handleSortChange.bind(this)
+        this.originalProducts = []
+
     }
 
-    handleAddProduct = (newProduct) => {
-        this.setState({products: [...this.state.products, newProduct]})
+    handleAddProduct(newProduct) {
+        this.setState({ products: [...this.state.products, newProduct] })
     }
 
-    handleDeleteProduct = (id) => {
+    handleDeleteProduct(id) {
         this.setState({
             products: this.state.products.filter((product) => product._id !== id)
         })
     }
 
-    handleUpdateProduct = (updatedProduct) => {
+    handleUpdateProduct(updatedProduct) {
         const updatedProducts = this.state.products.map((product) =>
             product._id === updatedProduct._id ? updatedProduct : product
         )
-        this.setState({products: updatedProducts})
-    }
-    // handleCategoryChange = (e) => {
-    //     let selectedCategory =  e.target.value
-    //     let selectedProducts = [...this.state.products]
-    //
-    //     if (selectedCategory !== "All") {
-    //         selectedProducts = selectedProducts.filter(products => products.includes(selectedCategory))
-    //     }
-    //     this.setState({selectedCategory, selectedProducts})
-    // }
-    //
-    // handleBrandChange = (e) => {
-    //     let selectedBrand =  e.target.value
-    //     let selectedProducts = [...this.state.products]
-    //
-    //     if (selectedBrand !== "All") {
-    //         selectedProducts = selectedProducts.filter(products => products.includes(selectedBrand))
-    //     }
-    //     this.setState({selectedBrand, selectedProducts})
-    // }
-
-    handleCategoryChange(event) {
-        this.setState({selectedCategory: event.target.value})
-    }
-
-    handleBrandChange(event) {
-        this.setState({selectedBrand: event.target.value})
+        this.setState({ products: updatedProducts })
     }
 
     componentDidMount() {
         axios.get(`${SERVER_HOST}/instruments`)
             .then(res => {
-
-                if (res.data) {
+                if ((res.data)) {
                     console.table(res.data)
 
-                    const categories = ["All", ...new Set(res.data.map(item => item.category).filter(Boolean))].sort()
-                    const brands = ["All", ...new Set(res.data.map(item => item.brand).filter(Boolean))].sort()
+                    this.originalProducts = res.data
+                    const categories = ["All", ...new Set(res.data.map(item => item.category).filter(Boolean))]
+                    const brands = ["All", ...new Set(res.data.map(item => item.brand).filter(Boolean))]
 
                     this.setState({
-
                         products: res.data,
-                        categories,
-                        brands,
-
+                        categories: categories,
+                        brands: brands
                     })
                 } else {
                     console.log("Record not found")
                 }
             })
-            .catch(error => console.error("Error fetching instruments:", error));
+            .catch(error => console.error("Error fetching instruments:", error))
+    }
 
+    handleCategoryChange(e) {
+        this.setState({ selectedCategory: e.target.value })
+    }
+
+    handleBrandChange(e) {
+        this.setState({ selectedBrand: e.target.value })
+    }
+
+    handleSortChange(order) {
+        this.setState({ sortOrder: order })
     }
 
     render() {
-        const {searchTerm, products, selectedBrand, selectedCategory} = this.state
+        const { searchTerm, products, selectedBrand, selectedCategory, sortOrder  } = this.state
 
-        const filteredProducts = products.filter(product => {
+        var filteredProducts = products.filter(product => {
             return (
-                ( product.name?.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                product.description?.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                product.brand?.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                product.price?.toString().includes(searchTerm) ||
-                product.rating?.toString().includes(searchTerm) ||
-                product.reviews?.toString().includes(searchTerm)) &&
-                product.category?.toLowerCase().includes(selectedCategory.toLowerCase()) &&
-                product.brand?.toLowerCase().includes(selectedBrand.toLowerCase()) )
-
+                (searchTerm === '' ||
+                    product.name?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                    product.description?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                    product.brand?.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                    product.price?.toString().includes(searchTerm) ||
+                    product.rating?.toString().includes(searchTerm) ||
+                    product.reviews?.toString().includes(searchTerm)) &&
+                (selectedCategory === "All" || product.category?.toLowerCase() === selectedCategory.toLowerCase()) &&
+                (selectedBrand === "All" || product.brand?.toLowerCase() === selectedBrand.toLowerCase())
+            )
         })
+
+        if (sortOrder === "nameAZ") {
+            filteredProducts.sort((a, b) => a.name.localeCompare(b.name))
+        } else if (sortOrder === "nameZA") {
+            filteredProducts.sort((a, b) => b.name.localeCompare(a.name))
+        } else if (sortOrder === "lowToHigh") {
+            filteredProducts.sort((a, b) => a.price - b.price)
+        } else if (sortOrder === "highToLow") {
+            filteredProducts.sort((a, b) => b.price - a.price)
+        } else if (sortOrder === "reviewsHighToLow") {
+            filteredProducts.sort((a, b) => b.reviews - a.reviews)
+        } else if (sortOrder === "reviewsLowToHigh") {
+            filteredProducts.sort((a, b) => a.reviews - b.reviews)
+        } else if (sortOrder === "default") {
+            filteredProducts = [...this.originalProducts]; // Reset to original order
+        }
+
         return (
             <div className="product-list">
                 <div className="searchBar">
@@ -120,36 +123,25 @@ export default class Products extends Component {
                         type="text"
                         placeholder="Search product name, price or description"
                         value={searchTerm}
-                        onChange={e => this.setState({searchTerm: e.target.value})}
+                        onChange={e => this.setState({ searchTerm: e.target.value })}
                     />
                 </div>
-                <CategoryDropDown categories={this.state.categories} handleCategoryChange={this.handleCategoryChange}/>
-                <BrandDropDown brands={this.state.brands} handleCategoryChange={this.handleCategoryChange}/>
 
-                <AddInstrument onAddProduct={this.handleAddProduct}/>
-                {/*<div className="grid">*/}
-                {/*    {this.state.products.map((product) => (*/}
-                {/*        <Instrument*/}
-                {/*            key={product._id}*/}
-                {/*            product={product}*/}
-                {/*            onDelete={this.handleDeleteProduct}*/}
-                {/*            onUpdate={this.handleUpdateProduct}*/}
-                {/*        />*/}
-                {/*    ))}*/}
-                {/*</div>*/}
-                <div className="grid">
-                    {filteredProducts.length > 0 ? (
-                        filteredProducts.map((product) => (
-                            <Instrument
-                                key={product._id}
-                                product={product}
-                                onDelete={this.handleDeleteProduct}
-                                onUpdate={this.handleUpdateProduct}
-                            />
-                        ))
-                    ) : (
-                        <p>No products found.</p>
-                    )}
+                <CategoryDropDown categories={this.state.categories} handleCategoryChange={this.handleCategoryChange} />
+                <BrandDropDown brands={this.state.brands} handleBrandChange={this.handleBrandChange} />
+
+                <AddInstrument onAddProduct={this.handleAddProduct} />
+                <div>
+                    <SortProducts sortOrder={this.state.sortOrder} handleSortChange={this.handleSortChange} />
+                    <div className="grid">
+                        {filteredProducts.length > 0 ? (
+                            filteredProducts.map(product => (
+                                <Instrument key={product._id} product={product} />
+                            ))
+                        ) : (
+                            <p>No products found.</p>
+                        )}
+                    </div>
                 </div>
             </div>
         )
